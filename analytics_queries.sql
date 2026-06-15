cb2c39ba8bf5950b5f0819d6e2ca1898ca5bb73d
Madhumitha Madduri <madhumithamadduri@gmail.com>
2026-06-15 09:37:23 +0530

    Update analytics_queries.sql

--- analytics_queries.sql
+++ analytics_queries.sql
@@ -11,3 +11,23 @@ SELECT MONTH(sale_date) AS month,
 SUM(total_amount) AS monthly_sales
 FROM sales
 GROUP BY MONTH(sale_date);
+-- Total Sales
+SELECT SUM(total_amount) AS total_sales
+FROM sales;
+
+-- Top Selling Product
+SELECT product_id, SUM(quantity) AS total_quantity
+FROM sales
+GROUP BY product_id
+ORDER BY total_quantity DESC;
+
+-- Monthly Sales
+SELECT MONTH(sale_date) AS month,
+SUM(total_amount) AS monthly_sales
+FROM sales
+GROUP BY MONTH(sale_date);
+
+-- Customer Orders
+SELECT customer_id, COUNT(*) AS orders_count
+FROM sales
+GROUP BY customer_id;
